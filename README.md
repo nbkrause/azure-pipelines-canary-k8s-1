5fe82137500346524762e52848728c583916fe89
Alvaro Saurin <alvaro.saurin@gmail.com>
2020-03-05 10:30:24 +0100

    Initial pipeline

---
+++ README.md
@@ -0,0 +1,102 @@
+# Canary deployments with Ambassador and Azure Pipelines
+
+## Rationale
+
+Azure pipelines have a deployment mechanism for doing _Canaries_. This mechanims can use
+an existing [_service mesh_](https://smi-spec.io/) in your cluster but, if you are not using
+Istio/Linkerd/etc., it does it with regular _Pods_: it creates a new `Deployment` with the same
+_labels_ but with the `image` being tested. Your existing `Service` will send traffic to the
+old pods as well as to the new pods (as they both match the Service `selector`), and this will
+be done proportionally to the number of replicas of each `Deployment`.
+
+The main problem with this solution is the granularity of the traffic split. A canary for 10%
+traffic would require 9 pods in the old deployment and 1 with the new one, something that is
+sometimes neither possible nor desirable.
+
+Ambassador can split traffic by assigning different weight to `Mappings`, as described
+[here](https://www.getambassador.io/reference/canary/). This provides much finer grain
+control over the traffic without the need to change the number of pods.
+
+## Proposed solution
+
+Instead of using the Azure pipelines facilities for splitting traffic with
+new Pods, we have created a new [`canarize.py`](deploy/canarize.py) script that,
+given the manifests with your `Service`/`Deployment`, a new `-canary` version of
+those will be created. In addition, it will also create a `Mapping` that will send
+traffic to the canary with the given _weight_.
+
+## References
+
+- [Canary deployment strategy for Kubernetes deployments](https://github.com/MicrosoftDocs/vsts-docs/blob/fc69779032416f5fe783409d25d75be372732640/docs/pipelines/ecosystems/kubernetes/canary-demo.md)
+- [Deployment strategies](https://github.com/microsoft/azure-pipelines-yaml/blob/master/design/deployment-strategies.md)
+- [Canary releases with Ambassador](https://www.getambassador.io/reference/canary/)
+
+## Overview of this repository
+
+* `./app`:
+
+  `app.py` - Simple Flask based web server instrumented using Prometheus instrumentation library for Python   applications. A custom counter is set up for the number of 'good' and 'bad' responses given out based on the value of `success_rate` variable.
+
+  `Dockerfile` - Used for building the image with each change made to `app.py`. With each change made to
+  `app.py`, build pipeline (CI) is triggered and the image gets built and pushed to the container registry.
+
+* `./manifests`:
+
+  `deployment.yml` - Contains specification of the `sampleapp` `Deployment` workload corresponding
+  to the image published earlier. This manifest file is used not just for the stable version of
+  `Deployment` object, but for deriving the `-canary` variant of the workloads as well.
+
+  `service.yml` - Creates a `sampleapp` `Service` for routing requests to the pods spun up by the
+  `Deployment` (stable and canary) mentioned above.
+
+* `./misc`:
+
+  `service-monitor.yml` - Used for setup of a ServiceMonitor object to set up Prometheus metric scraping.
+
+  `fortio-deploy.yml` - Used for setup of fortio deployment that is subsequently used as a
+  load-testing tool to send a stream of requests to the sampleapp service deployed earlier.
+  With sampleapp service's selector being applicable for all the three pods resulting from
+  the Deployment objects that get created during the course of this how-to guide - sampleapp,
+  sampleapp-baseline and sampleapp-canary, the stream of requests sent to sampleapp get
+  routed to pods under all these three deployments.
+
+## Preparing your cluster
+
+* Create a Kubernetes cluster in Azure by following the
+  [official docs](https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough-portal).
+  We will assume your have created it in a _Resource group_ called `Ambassador-Azure-Pipeline`,
+  and the cluster name will be `Ambassador-Azure-Pipeline`.
+* Get the credentials for your Kuberentes cluster (using the _resource group_ and the _cluster name_):
+  ```shell script
+  $ az aks get-credentials --resource-group Ambassador-Azure-Pipeline --name Ambassador-Azure-Pipeline
+  ```
+* Install Ambassador int this cluster by following [the instructions](https://www.getambassador.io/user-guide/install/). For example,
+  ```shell script
+  helm repo add datawire https://www.getambassador.io
+  kubectl create namespace ambassador
+  helm install ambassador --namespace ambassador datawire/ambassador
+  AMB_IP=$(kubectl get -n ambassador service ambassador -o 'go-template={{range .status.loadBalancer.ingress}}{{print .ip "\n"}}{{end}}')
+  edgectl login -n ambassador $AMB_IP
+  ```
+* Install the prometheus operator. You can do it with Helm 3, with:
+  ```shell script
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm install prometheus bitnami/prometheus-operator
+  ```
+  (You can access it with `kubectl port-forward --namespace default svc/prometheus-prometheus-oper-prometheus 9090:9090`)
+
+## Preparing the repo
+
+### Connections
+
+* Go to `https://dev.azure.com/<user>/<project>/_settings/adminservices` and:
+  - Create a new _Docker Registry_ connection, select `Azure Container Registry` and select
+    one of the existing registries. Assign a name like `azurepipelinescanaryk8s`
+  - Add another _Kubernetes_ service connection for connecting to your existing
+    kubernetes cluster. Name it `k8sEnvironment`.
+
+### Customizing the pipeline
+
+* Review the variables in the `azure-pipelines.yml`, specially the `containerRegistry` and `environment`.
+  Their values should match the service connections created previously.
+* In `manifests/deployment.yml`, replace the `image` with your container registry's URL.
---
+++ deploy/canarize.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+
+import argparse
+import os.path
+import copy
+import sys
+from os import path
+
+
+# hack for installing some package if it is not available
+def install_and_import(importname, package):
+    import importlib
+    try:
+        importlib.import_module(importname)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        return importlib.import_module(importname)
+
+
+yaml = install_and_import("yaml", "pyyaml")
+
+
+# the suffix added to all the resources
+DEFAULT_CANARY_SUFFIX = "-canary"
+
+# default Prefix for the Mappings to the Service
+DEFAULT_MAPPINGS_PREFIX = "/"
+
+
+def image_tag(name):
+    try:
+        return name.split(":")[-1]
+    except:
+        return ""
+
+
+def image_except_tag(name):
+    try:
+        ":".join(name.split(":")[:-1])
+    except:
+        return name
+
+
+def image_replace_tag(name, new_tag):
+    try:
+        ":".join([image_except_tag(name), new_tag])
+    except:
+        return name
+
+
+def gen_mapping(args, service, weight=None):
+    """
+    Generate a Mapping for a service/prefix and (optional) weight
+    """
+    prefix = args.prefix
+    mapping = {
+        "apiVersion": "getambassador.io/v1",
+        "kind":  "Mapping",
+        "metadata": {
+            "name":  f"mapping-for-{service}"
+        },
+        "spec": {
+            "prefix": prefix,
+            "service": service
+        }
+    }
+
+    if args.namespace:
+        mapping["metadata"]["namespace"] = args.namespace
+
+    if weight:
+        mapping["spec"]["weigth"] = weight
+
+    return mapping
+
+
+def canarize_deployment(args, input_yaml):
+    """
+    Create a canary for an existing Deployment.
+    We do this by:
+    - adding a '-canary' suffix to the name of the Deployment
+    - adding a '-canary' suffix to all the labels in the Service selector
+      as well as in the Pods template.
+    """
+
+    # append the -canary to the Deployment name
+    output_yaml = copy.deepcopy(input_yaml)
+    canary_deployment_name = input_yaml["metadata"]["name"] + args.suffix
+    output_yaml["metadata"]["name"] = canary_deployment_name
+
+    print(f"# Creating canary Deployment {canary_deployment_name}")
+
+    # append the -canary to all the labels in the selector
+    try:
+        for (k, v) in input_yaml["spec"]["selector"]["matchLabels"].items():
+            output_yaml["spec"]["selector"]["matchLabels"][k] = v + \
+                args.suffix
+    except IndexError:
+        pass
+
+    for (k, v) in input_yaml["spec"]["template"]["metadata"]["labels"].items():
+        output_yaml["spec"]["template"]["metadata"]["labels"][k] = v + args.suffix
+
+    if args.image:
+        for container in output_yaml["spec"]["template"]["spec"]["containers"]:
+            if image_except_tag(container["image"]) == image_except_tag(args.image):
+                print(f"# Replacing Deployment image {args.image}")
+                container["image"] = args.image
+
+    if args.namespace:
+        output_yaml["metadata"]["namespace"] = args.namespace
+
+    return [output_yaml]
+
+
+def canarize_service(args, input_yaml):
+    """
+    Create a canary for an existing Service.
+    We do this by:
+    - adding a '-canary' suffix to the name of the Service
+    - adding a '-canary' suffix to all the labels in the Service selector
+    """
+    res = []
+
+    # append the -canary to the Service name
+    output_yaml = copy.deepcopy(input_yaml)
+    canary_service_name = input_yaml["metadata"]["name"] + args.suffix
+    output_yaml["metadata"]["name"] = canary_service_name
+
+    print(f"# Creating canary Service {canary_service_name}")
+
+    # append the -canary to all the labels in the selector
+    for (k, v) in input_yaml["spec"]["selector"].items():
+        output_yaml["spec"]["selector"][k] = v + args.suffix
+
+    if args.namespace:
+        output_yaml["metadata"]["namespace"] = args.namespace
+
+    res += [output_yaml]
+
+    # (maybe) create the Mappings for splitting traffic to the base/canary services
+    if args.gen_mapping:
+        base_service_name = input_yaml["metadata"]["name"]
+        print(f"# Creating Mapping for {base_service_name}")
+        res += [gen_mapping(args, base_service_name)]
+
+    if args.gen_mapping or args.gen_mapping_canary:
+        canary_service_name = output_yaml["metadata"]["name"]
+        print(
+            f"# Creating Mapping for Service {canary_service_name} (weight: {args.canary_weight})")
+        res += [gen_mapping(args, canary_service_name,
+                            weight=args.canary_weight)]
+
+    return res
+
+
+def canarize(args, input_yaml):
+    """
+    Create a canary for existing resources
+    """
+    if input_yaml["kind"] == "Deployment":
+        return canarize_deployment(args, input_yaml)
+    elif input_yaml["kind"] == "Service":
+        return canarize_service(args, input_yaml)
+    else:
+        return input_yaml
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Canarize a set of files.")
+    parser.add_argument("--suffix", "-s", default=DEFAULT_CANARY_SUFFIX,
+                        help="suffix for the resources names")
+    parser.add_argument("--prefix", "-P",
+                        default=DEFAULT_MAPPINGS_PREFIX,
+                        help="prefix for all the Mappings")
+    parser.add_argument("--gen-mapping", "-M",
+                        action="store_true",
+                        default=False,
+                        help="generate a Mapping for the canary as well as for the base service")
+    parser.add_argument("--gen-mapping-canary", "-m",
+                        action="store_true",
+                        default=False,
+                        help="generate a Mapping only for the canary")
+    parser.add_argument("--debug",
+                        action="store_true",
+                        default=False,
+                        help="enable some debugging")
+    parser.add_argument("--namespace", "-n",
+                        default=None,
+                        help="namespace")
+    parser.add_argument("--image", "-I",
+                        default=None,
+                        help="image to use in the deployment")
+    parser.add_argument("--canary-weight", "-w",
+                        default=None,
+                        type=int,
+                        help="weight for the service")
+    parser.add_argument("--output", "-o",
+                        metavar="FILE", type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help="output file")
+    parser.add_argument("files", metavar="FILE", type=argparse.FileType('r'),
+                        default=sys.stdin, nargs='+', help="a manifest to process")
+
+    args = parser.parse_args()
+
+    if not args.suffix.startswith("-"):
+        args.suffix = "-" + args.suffix
+
+    res = []
+    for f in args.files:
+        for s in f.read().split("---"):
+            if not s:
+                continue
+
+            try:
+                in_yaml = yaml.safe_load(s)
+                res += canarize(args, in_yaml)
+
+            except yaml.YAMLError as exc:
+                print(f"Error parsing manifest {f}:")
+                print(exc)
+
+    print(f"# Writting to {args.output.name}")
+    res_str = "---\n" + "---\n".join([yaml.dump(x) for x in res])
+
+    args.output.write(res_str)
+
+    if args.debug and not ("stdout" in args.output.name):
+        print(f"# File contents:")
+        print(res_str)
